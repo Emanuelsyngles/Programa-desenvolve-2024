388f73c0c1b1cccb7ae8d98f85e1b9ddb0e0caa6
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-05-17 15:50:02 -0300

    Criptografia.

--- Grupo oboticário/criptografia/encriptacaoAssimetrica/encriptacaoAssimetric.js
+++ Grupo oboticário/criptografia/encriptacaoAssimetrica/encriptacaoAssimetric.js
@@ -1,1 +1,25 @@
 import { generateKeyPairSync } from 'crypto'
+
+
+const { privateKey, publicKey } = generateKeyPairSync('rsa',{
+        modulusLength: 2048,
+
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'pem',
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'pem',
+        },
+})
+
+//console.log(publicKey)
+//console.log(privateKey)
+
+import { publicEncrypt, privateDecrypt} from 'crypto'
+
+const dadosCriptografas = publicEncrypt(
+    publicKey,
+    Buffer.from("Mensagem super secreta")
+)
